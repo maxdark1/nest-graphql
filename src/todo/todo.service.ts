0755b04808e561ef8179787bb9f063a54db615cc
Juan ramon Lara montoya <juan.lara@creatoris.com.mx>
2024-03-03 15:33:53 -0600

    Avances del curso

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
 import { Todo } from './entities/todo.entity';
@@ -9,19 +9,27 @@ export class TodoService {
   private todos: Todo[] = [
     {id:1 , description: "Piedra del Alma", done: false},
     {id:2 , description: "Piedra del Tiempo", done: false},
-    {id:3 , description: "Piedra del Espacio", done: false}
+    {id:3 , description: "Piedra del Espacio", done: true}
   ];
 
   create(createTodoDto: CreateTodoDto) {
-    return 'This action adds a new todo';
+    let todo = {
+      id:4,
+      description: createTodoDto.descripcion,
+      done: false
+    };
+    this.todos.push(todo);
+    return todo;
   }
 
-  findAll() {
-    return `This action returns all todo`;
+  findAll() : Todo []{
+    return this.todos;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} todo`;
+  findOne(id: number) : Todo{
+    const todo = this.todos.find(todo => todo.id === id);
+    if(!todo) throw new NotFoundException(`No se encontro el elemento ${id}`);
+    return todo;
   }
 
   update(id: number, updateTodoDto: UpdateTodoDto) {
